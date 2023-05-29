d23530f6d32f57d8ddd539aa3317d445c1133c9b
alkher27 <alikheraj@gmail.com>
2023-05-29 18:36:37 +0000

    save the forms (posts) - params in controller

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,2 +1,10 @@
 class PostsController < ApplicationController
+
+    def create
+        post = Post.new
+        post["body"] = params["body"]
+        post["user_id"] = current_user["id"]
+        post.save
+        redirect_to "/posts"
+    end
 end
