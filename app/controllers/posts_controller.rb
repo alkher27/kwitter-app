569253b773fa199676858de328bc6335205f844c
alkher27 <alikheraj@gmail.com>
2023-05-29 18:41:30 +0000

    post body replaced in card text

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,10 @@
 class PostsController < ApplicationController
 
+def index
+    @posts = Post.all.order("created_at desc")
+end
+
+
     def create
         post = Post.new
         post["body"] = params["body"]
