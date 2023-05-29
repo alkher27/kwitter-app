19b9bdc3cf9c8687531e35a151034e99f3a1d917
alkher27 <alikheraj@gmail.com>
2023-05-29 18:13:08 +0000

    posts

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   # Resources
   resources "sessions"
   resources "users"
+  resources "posts"rail
   
   # Login/out
   get "/login", :controller => "sessions", :action => "new"
